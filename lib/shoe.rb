ffb1af7fbc88b2bdae0db3c45cfa40950cbafce7
Tara M. Harwood <tara.m.harwood@gmail.com>
2021-02-05 11:35:53 -0800

    commit tree 1c596bc809a446616a1700943e3ef655dfd310b6

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+class Shoe
+
+    # first up are the attribute accessors, we only really need accessir and reader, bc who needs write-only?
+
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+    
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end    
+
+end    
